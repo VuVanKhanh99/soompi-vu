386bc9ec0f3e49400eaaf7c4163c9c0c6ca87c1c
VuVanKhanh99 <vanvukhanh1999@gmail.com>
2022-07-10 23:24:21 +0700

    update-new-code suspense

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -1,18 +1,15 @@
 import React, { lazy, Suspense } from 'react'
-import { Switch, useRouteMatch, Route } from 'react-router-dom'
-//import Homepage from '../../features/Homepage/components'
-import VideoPage from '../../features/VideoPageItem'
-import ViewArticle from '../Common/ViewArticle'
-//import PageNotFound from '../PageNotFound'
+import { Switch } from 'react-router-dom'
 import RouteAuth from '../RouteAuth'
 import RouteLayout from '../RouteLayout'
-import ScrollToTop from '../ScrollToTop'
 import { Spin } from 'antd'
 
 export default function LayoutFeature() {
   const Homepage = lazy(() => import('../../features/Homepage/components'))
   const VideoPage = lazy(() => import('../../features/VideoPageItem'))
   const PageNotFound = lazy(() => import('../PageNotFound'))
+  const ViewArticle = lazy(()=> import('../Common/ViewArticle'))
+
   return (
     <>
       <Suspense fallback={<Spin size={'large'} />}>
