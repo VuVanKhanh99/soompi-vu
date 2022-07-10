917a5987347bf697b1aa9e0ac4d58d3f3d8da748
VuVanKhanh99 <vanvukhanh1999@gmail.com>
2022-07-10 23:40:25 +0700

    update new code

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -1,25 +1,19 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import { Switch } from 'react-router-dom'
+import Homepage from '../../features/Homepage/components'
+import VideoPage from '../../features/VideoPageItem'
+import ViewArticle from '../Common/ViewArticle'
+import PageNotFound from '../PageNotFound'
 import RouteAuth from '../RouteAuth'
 import RouteLayout from '../RouteLayout'
-import { Spin } from 'antd'
 
 export default function LayoutFeature() {
-  const Homepage = lazy(() => import('../../features/Homepage/components'))
-  const VideoPage = lazy(() => import('../../features/VideoPageItem'))
-  const PageNotFound = lazy(() => import('../PageNotFound'))
-  const ViewArticle = lazy(()=> import('../Common/ViewArticle'))
-
   return (
-    <>
-      <Suspense fallback={<Spin size={'large'} />}>
-        <Switch>
-          <RouteLayout exact path="/" component={Homepage} />
-          <RouteLayout path="/category/video" component={VideoPage} />
-          <RouteLayout path="/article/:id/:title" component={ViewArticle} />
-          <RouteAuth component={PageNotFound} />
-        </Switch>
-      </Suspense>
-    </>
+    <Switch>
+      <RouteLayout exact path="/" component={Homepage} />
+      <RouteLayout path="/category/video" component={VideoPage} />
+      <RouteLayout path="/article/:id/:title" component={ViewArticle} />
+      <RouteAuth component={PageNotFound} />
+    </Switch>
   )
 }
